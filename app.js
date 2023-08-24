bf39bb455fd933e853ede80f4000791d3163243c
Didem Cakir <didemydn@gmail.com>
2023-08-24 23:58:35 +0200

    recipe db done

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ app.use("/api", indexRoutes);
 const authRouter = require("./routes/auth.routes");
 app.use("/auth", authRouter);
 
+const recipeRouter = require("./routes/recipe.routes");
+app.use("/recipe", recipeRouter);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
---
+++ routes/recipe.routes.js
@@ -0,0 +1,5 @@
+const Recipe = require("../models/Recipe.model");
+const router = require("express").Router();
+
+
+module.exports = router;
