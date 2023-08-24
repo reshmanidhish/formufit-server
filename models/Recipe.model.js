4323e4b2d8e88909ea6fa296781cf2dbcf969f97
Didem Cakir <didemydn@gmail.com>
2023-08-25 00:15:25 +0200

    recipe db corrected

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -26,9 +26,11 @@ const recipeSchema = new Schema(
     }
 )
 
+const MONGO_URI =
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/formufit";
 
 // Connect to MongoDB and Atlas
-mongoose.connect(MONGODB_URI)
+mongoose.connect(MONGO_URI)
 .then(() => {
   console.log("Connected to MongoDB Atlas");
 
