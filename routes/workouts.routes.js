ed6cb126976093a76d938be41c0e0366a4069d9b
Reshma Nidhish <reshmanidhish0@gmail.com>
2023-09-01 12:31:42 +0200

    workout routes added

--- routes/workouts.routes.js
+++ routes/workouts.routes.js
@@ -1,16 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const Fitness = require('../models/Fitness.model');
 
-const workouts = []; // Store uploaded workouts here
+
+
 
 router.post('/upload', (req, res) => {
-  const { name, videoUrl,bodyType } = req.body;
-  workouts.push({ name, videoUrl ,bodyType});
-  res.status(201).json({ message: 'Workout uploaded successfully' });
+  const { title, videoUrl, bodyType } = req.body;
+
+ 
+  const newWorkout = new Fitness({ title, videoUrl, bodyType });
+  
+  
+  newWorkout.save()
+    .then(() => {
+      res.status(201).json({ message: 'Workout uploaded successfully' });
+    })
+    .catch((error) => {
+      console.log(error)
+      res.status(500).json({ error: 'Failed to upload workout' });
+    });
 });
 
 router.get('/', (req, res) => {
-  res.json(workouts);
+
+  Fitness.find()
+    .then((workouts) => {
+      res.json(workouts);
+    })
+    .catch((error) => {
+   
+      res.status(500).json({ error: 'Failed to retrieve workouts' });
+    });
 });
 
 module.exports = router;
