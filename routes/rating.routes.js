d0d305ef7c55695d6572804511532d5b42072b89
Didem Cakir <didemydn@gmail.com>
2023-09-09 15:05:29 +0200

    star rating done

--- routes/rating.routes.js
+++ routes/rating.routes.js
@@ -6,8 +6,8 @@ router.post('/', async (req, res) => {
         const {recipeId, rating} = req.body;
         const userId = req.payload;
     
-        const newComment = await Comment.create({user: userId, recipe: recipeId, rating});
-        res.status(201).json(newComment); 
+        const newRate = await Rating.create({user: userId, recipe: recipeId, rating});
+        res.status(201).json(newRate); 
     }
     catch (error) {
         res.status(500).json({error: "Error creating a rate"});
